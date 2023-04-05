e6eb4fcc99b0fe7631a9bd59ae5d69288b72fd85
varshani reddy <varshanireddy@client-10-228-7-48.tamulink.tamu.edu>
2023-04-05 16:42:40 -0500

    add code

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './HomePage.css';
+
+function HomePage() {
+  return (
+    <div className="container">
+      <h1>Howdy!! Welcome to my page.. </h1>
+      <p>This is my page about wpaskjbdn zqnakjsnde.</p>
+      <p1> I am Varshani Reddy Patlolla.</p1>
+      <div className="button-container">
+        <input type="text" placeholder="Search" className="search-bar" />
+        <button className="button">doesn't</button>
+        <button className="button button-right">work</button>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
